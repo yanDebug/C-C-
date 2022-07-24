165d73fb60bb3fb20c14e583657a5d749bb65ac1
颜子 <81792218+yanDebug@users.noreply.github.com>
2022-07-24 20:42:49 +0800

    添加项目文件。

---
+++ 2/2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    printf("Hello,world\n");
+
+    system("pause");    // 防止运行后自动退出，需头文件stdlib.h
+
+    return 0;
+}
+
+//int 函数返回类型
+//main 函数名
+//{} 函数体
+//printf 输出函数，库函数，需要引用头文件stdio.h
+//运行代码 F5
